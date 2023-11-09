1d40a69755a7a726b4f21ea0710a53a07033d484
Mihail <infast1k@mail.ru>
2023-11-09 17:50:47 +0300

    create models; add into admin

--- server/relationships/admin.py
+++ server/relationships/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.RelativeStatus)
+admin.site.register(models.AbstractProfile)
+admin.site.register(models.Relatives)
--- server/relationships/models.py
+++ server/relationships/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class RelativeStatus(models.Model):
+    status_name = models.CharField(max_length=25, null=False, unique=True)
+
+    def __str__(self) -> str:
+        return self.status_name
+
+    class Meta:
+        verbose_name = "relative status"
+        verbose_name_plural = "relative statuses"
+
+
+class AbstractProfile(models.Model):
+    last_name = models.CharField(max_length=25, null=False, blank=False)
+    first_name = models.CharField(max_length=25, null=False, blank=False)
+    patronymic = models.CharField(max_length=25, null=True, blank=True)
+    phone = models.CharField(max_length=15, null=False, blank=False, unique=True)
+    birth_date = models.DateField(null=False)
+    address = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name} {self.patronymic}"
+
+    class Meta:
+        verbose_name = "abstract profile"
+        verbose_name_plural = "abstract profiles"
+
+
+class Relatives(models.Model):
+    user = models.ForeignKey("users.User", null=False, on_delete=models.CASCADE)
+    abstract_profile = models.ForeignKey("relationships.AbstractProfile", null=False, on_delete=models.CASCADE)
+    status = models.ForeignKey("relationships.RelativeStatus", null=False, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.abstract_profile
+
+    class Meta:
+        verbose_name = "relative"
+        verbose_name_plural = "relitives"
