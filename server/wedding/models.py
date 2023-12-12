8f57541f01512678244622beba7804ca140d3482
Mihail <infast1k@mail.ru>
2023-12-12 23:34:35 +0300

    create wedding, wedding_status models

--- server/wedding/models.py
+++ server/wedding/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Wedding(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE)
+    change_last_name = models.BooleanField(null=False, default=True)
+    status = models.ForeignKey('wedding.WeddingStatus', on_delete=models.CASCADE, default=1)
+    event_datatime = models.DateTimeField(blank=False, null=False)
+
+
+class WeddingStatus(models.Model):
+    status_name = models.CharField(max_length=50, null=False, unique=True)
+
+    def __str__(self) -> str:
+        return self.status_name
+    
+    class Meta:
+        verbose_name = "wedding status"
+        verbose_name_plural = "wedding statuses"
+
+
+class Witnesses(models.Model):
+    """Таблица для свидетелeй"""
+    ...
